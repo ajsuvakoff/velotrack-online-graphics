cdd1d6b1d0adc41d83f1a32b6d84724bf486af60
roman.ajsuvakov <roman.ajsuvakov@jugru.org>
2023-02-11 00:06:31 +0300

    git CI try

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/velotrack-online-graphics/'
+        : '/',
     pages: {
         index: {
             entry: 'src/client/src/main.js',
             template: 'src/client/public/index.html'
         }
     },
+
     outputDir: "dist/client"
 }
